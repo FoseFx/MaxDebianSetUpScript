8db0f133ebe896a0df16e3d24ae6cb4a17394996
FoseFx <info@fosefx.de>
2018-08-13 22:40:48 +0200

    idgf

--- public.sh
+++ public.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
+key=$1
 apt-get install curl
-curl -H 'Authorization: token $1' -H -O -L 'Accept: application/vnd.github.v3.raw' https://raw.githubusercontent.com/FoseFx/MaxDebianSetUpScript/master/setup.sh
-bash setup.sh $1
-
-# https://pastebin.com/raw/HNXqjr23
+curl -H 'Authorization: token $key' -O -L 'Accept: application/vnd.github.v3.raw' https://raw.githubusercontent.com/FoseFx/MaxDebianSetUpScript/master/setup.sh
+bash setup.sh "$key"
--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+key=$1
+
 apt-get update
 apt-get upgrade
 
@@ -18,14 +20,14 @@ mkdir /home/max/.ssh
 
 chmod 700 /home/max/.ssh
 
-curl -H 'Authorization: token $1' -O -L 'Accept: application/vnd.github.v3.raw' https://raw.githubusercontent.com/FoseFx/MaxDebianSetUpScript/master/authorized_keys
+curl -H 'Authorization: token $key' -O -L 'Accept: application/vnd.github.v3.raw' https://raw.githubusercontent.com/FoseFx/MaxDebianSetUpScript/master/authorized_keys
 cat authorized_keys >> /home/max/.ssh/authorized_keys
 
 # replace with download to github repo :D
 chmod 400 /home/deploy/.ssh/authorized_keys
 chown max:max /home/max -R
 
-curl -H 'Authorization: token $1' -O -L 'Accept: application/vnd.github.v3.raw' https://raw.githubusercontent.com/FoseFx/MaxDebianSetUpScript/master/sshd_config
+curl -H 'Authorization: token $key' -O -L 'Accept: application/vnd.github.v3.raw' https://raw.githubusercontent.com/FoseFx/MaxDebianSetUpScript/master/sshd_config
 cat sshd_config >> /etc/ssh/sshd_config
 
 systemctl restart ssh
@@ -33,7 +35,7 @@ systemctl restart ssh
 ufw allow 22
 ufw enable
 
-curl -H 'Authorization: token $1' -O -L 'Accept: application/vnd.github.v3.raw' https://raw.githubusercontent.com/FoseFx/MaxDebianSetUpScript/master/00logwatch
+curl -H 'Authorization: token $key' -O -L 'Accept: application/vnd.github.v3.raw' https://raw.githubusercontent.com/FoseFx/MaxDebianSetUpScript/master/00logwatch
 cat 00logwatch >> /etc/cron.daily/00logwatch
 
 
