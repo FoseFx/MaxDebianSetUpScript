be08d382662fa2a32de2b647a8f1559ed775bd13
FoseFx <info@fosefx.de>
2018-08-13 22:27:39 +0200

    ´fuck

--- setup.sh
+++ setup.sh
@@ -18,14 +18,14 @@ mkdir /home/max/.ssh
 
 chmod 700 /home/max/.ssh
 
-curl -H 'Authorization: token $1' -H -O -L 'Accept: application/vnd.github.v3.raw' https://raw.githubusercontent.com/FoseFx/MaxDebianSetUpScript/master/authorized_keys
+curl -H 'Authorization: token $1' -O -L 'Accept: application/vnd.github.v3.raw' https://raw.githubusercontent.com/FoseFx/MaxDebianSetUpScript/master/authorized_keys
 cat authorized_keys >> /home/max/.ssh/authorized_keys
 
 # replace with download to github repo :D
 chmod 400 /home/deploy/.ssh/authorized_keys
 chown max:max /home/max -R
 
-curl -H 'Authorization: token $1' -H -O -L 'Accept: application/vnd.github.v3.raw' https://raw.githubusercontent.com/FoseFx/MaxDebianSetUpScript/master/sshd_config
+curl -H 'Authorization: token $1' -O -L 'Accept: application/vnd.github.v3.raw' https://raw.githubusercontent.com/FoseFx/MaxDebianSetUpScript/master/sshd_config
 cat sshd_config >> /etc/ssh/sshd_config
 
 systemctl restart ssh
@@ -33,7 +33,7 @@ systemctl restart ssh
 ufw allow 22
 ufw enable
 
-curl -H 'Authorization: token $1' -H -O -L 'Accept: application/vnd.github.v3.raw' https://raw.githubusercontent.com/FoseFx/MaxDebianSetUpScript/master/00logwatch
+curl -H 'Authorization: token $1' -O -L 'Accept: application/vnd.github.v3.raw' https://raw.githubusercontent.com/FoseFx/MaxDebianSetUpScript/master/00logwatch
 cat 00logwatch >> /etc/cron.daily/00logwatch
 
 
